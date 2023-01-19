ba5019d75b9bbf17c517b370ec4e58b20c182c3f
jhon <jhonm9696@gmail.com>
2023-01-18 19:03:37 -0500

    funciones de CREAR,LISTAR y ELIMINAR Productos, ya terminados y funcionando.

--- src/main/java/Bodega/com/example/demo/controller/InventarioController.java
+++ src/main/java/Bodega/com/example/demo/controller/InventarioController.java
@@ -1,12 +1,23 @@
 package Bodega.com.example.demo.controller;
+
 import Bodega.com.example.demo.model.Model;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.core.ReactiveMongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.server.RouterFunction;
+import static org.springframework.web.reactive.function.server.RequestPredicates.DELETE;
+import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
 import static org.springframework.web.reactive.function.server.RequestPredicates.POST;
 import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Flux;
+
+
+
+
 import static org.springframework.web.reactive.function.server.RequestPredicates.accept;
 import static org.springframework.web.reactive.function.server.RouterFunctions.route;
 
@@ -14,21 +25,44 @@ import static org.springframework.web.reactive.function.server.RouterFunctions.r
 @Configuration
 public class InventarioController {
 
-    private  ReactiveMongoTemplate template;
+    private ReactiveMongoTemplate template;
 
     public InventarioController(ReactiveMongoTemplate template) {
         this.template = template;
     }
 
 
-
     @Bean
     public RouterFunction<ServerResponse> createCategory() {
         return route(
-                POST("/test/create/").and(accept(MediaType.APPLICATION_JSON)),
-                request -> template.save(request.bodyToMono(Model.class), "TESTDEPRUEBA")
+                POST("/create/").and(accept(MediaType.APPLICATION_JSON)),
+                request -> template.save(request.bodyToMono(Model.class), "Products")
                         .then(ServerResponse.ok().build())
         );
     }
 
+    @Bean
+    public RouterFunction<ServerResponse> listarCategory() {
+        return route(
+                GET("/list/").and(accept(MediaType.APPLICATION_JSON)),
+                request -> template.findAll((Model.class), "Products").collectList()
+                        .flatMap(list -> ServerResponse.ok()
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .body(BodyInserters.fromPublisher(Flux.fromIterable(list), Model.class)))
+        );
+    }
+
+
+    @Bean
+    public RouterFunction<ServerResponse> DeleteCategory() {
+        return route(
+                DELETE("/delete/{name}").and(accept(MediaType.APPLICATION_JSON)),
+                request -> template.findAndRemove(findProduct(request.pathVariable("name")), (Model.class), "Products")
+                        .then(ServerResponse.ok().build())
+        );
+    }
+
+    private Query findProduct(String name) {
+        return new Query(Criteria.where("nombre").is(name));
+    }
 }
--- src/main/java/Bodega/com/example/demo/controller/VentasController.java
+++ src/main/java/Bodega/com/example/demo/controller/VentasController.java
@@ -1,4 +1,35 @@
 package Bodega.com.example.demo.controller;
 
+import Bodega.com.example.demo.model.Model;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.core.ReactiveMongoTemplate;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.BodyInserters;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Flux;
+
+import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
+import static org.springframework.web.reactive.function.server.RequestPredicates.accept;
+import static org.springframework.web.reactive.function.server.RouterFunctions.route;
+@Configuration
 public class VentasController {
+    private ReactiveMongoTemplate template;
+
+    public VentasController(ReactiveMongoTemplate template) {
+        this.template = template;
+    }
+
+
+    @Bean
+    public RouterFunction<ServerResponse> History() {
+        return route(
+                GET("/history/").and(accept(MediaType.APPLICATION_JSON)),
+                request -> template.findAll((Model.class), "Sales").collectList()
+                        .flatMap(list -> ServerResponse.ok()
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .body(BodyInserters.fromPublisher(Flux.fromIterable(list), Model.class)))
+        );
+    }
 }
